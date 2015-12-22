08c21bc9bd426ed51c7c4ce32881c20acb741419
Josh Draper <mrjacoste@gmail.com>
2015-12-22 10:49:58 -0500

    Rebase

---
+++ main.py
@@ -0,0 +1,19 @@
+import smtplib, random, string
+from config import *
+def random_word(length):
+   return ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for i in range(length))
+
+sms_text=""
+numbers = [line.rstrip('\n') for line in open('numbers.txt')]
+facts = [line.rstrip('\n') for line in open('facts.txt')]
+for number in numbers:
+    sms_text=number + "\n" + random.choice(facts) + "\n<To cancel Daily Cat Facts, reply 'Cancel " + random_word(20) + "'>"
+    # server = smtplib.SMTP( "smtp.gmail.com", 587 )
+    # server.starttls()
+    # server.login( gmail_username, gmail_password)
+    # server.sendmail( gmail_username, number, sms_text)
+    # server.quit()
+    print(sms_text);
+
+
+
